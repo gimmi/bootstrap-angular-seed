9152bf23e72bde01f894ae35b1f1d86cca821e2e
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-12-15 21:46:34 +0100

    $http call mock sample

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+/* global angular */
+
 'use strict';
  
 angular.module('app', []);
  
-angular.module('app').factory('appSvc', function () {
-    return {
-    	a: 1
-    };
+angular.module('app').factory('appServerSvc', function ($http) {
+	return {
+		call: function () {
+			return $http.get('/login').then(function (ret) {
+				return ret.headers('X-User-Name');
+			});
+		}
+	};
 });
 
 angular.module('app').controller('appFeedbackCtrl', function ($scope) {
--- tests.js
+++ tests.js
@@ -17,3 +17,26 @@ describe('appFeedbackCtrl', function () {
 		expect(scope.text).toBe('ciao');
 	});
 });
+
+describe('appServerSvc', function () {
+	var svc, httpBackend;
+
+	beforeEach(module('app'));
+
+	beforeEach(inject(function ($httpBackend, appServerSvc) {
+		httpBackend = $httpBackend;
+		svc = appServerSvc;
+	}));
+
+	it('should return username', function () {
+		httpBackend.when('GET', '/login').respond({userId: 'userX'}, {'X-User-Name': 'xxx'});
+
+		var actual;
+		svc.call().then(function (ret) {
+			actual = ret;
+		});
+
+		httpBackend.flush();
+		expect(actual).toBe('xxx');
+	});
+});
