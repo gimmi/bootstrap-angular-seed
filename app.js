dd6513159340108dd6b74cfd520c12260f357fa7
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-12-15 11:00:18 +0100

    sample controller test

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
  
 angular.module('app', []);
  
-angular.module('app').factory('appSvc', function() {
+angular.module('app').factory('appSvc', function () {
     return {
     	a: 1
     };
+});
+
+angular.module('app').controller('appFeedbackCtrl', function ($scope) {
+	$scope.text = 'ciao';
 });
--- tests.js
+++ tests.js
@@ -1,9 +1,19 @@
+/* global describe, beforeEach, it, expect */
+/* global module, inject */
+
 'use strict';
 
-describe('true', function() {
-	beforeEach(angular.mock.module('app'));
+describe('appFeedbackCtrl', function () {
+	var scope, ctrl;
 
-	it('should be true', function() {
-		expect(true).toBeTruthy();
+	beforeEach(module('app'));
+
+	beforeEach(inject(function ($rootScope, $controller) {
+	  scope = $rootScope.$new();
+	  ctrl = $controller('appFeedbackCtrl', { $scope: scope });
+	}));
+
+	it('should be true', function () {
+		expect(scope.text).toBe('ciao');
 	});
 });
