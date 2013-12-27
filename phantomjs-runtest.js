3e846ed0dca87af7218fe4d03f6425d19be50df6
Gian Marco Gherardi <gianmarco.gherardi@gmail.com>
2013-12-27 18:50:06 +0100

    Refactoring

--- phantomjs-runtest.js
+++ phantomjs-runtest.js
@@ -1,15 +1,19 @@
+/* jshint phantom: true */
+/* global jsApiReporter */
+
+'use strict';
+
 var args = require('system').args,
 	page = require('webpage').create(),
 	checkLoop = function () {
 		var jasmineStatus = page.evaluate(function () { return jsApiReporter.status(); });
-		if (jasmineStatus == 'done') {
-			var jasmineSpecs = page.evaluate(function () { return jsApiReporter.specs(); });
-			var jasmineFailedSpecs = jasmineSpecs.filter(function (spec) { return spec.status == 'failed'; });
-			console.log(jasmineSpecs.length, ' specs executed, ', jasmineFailedSpecs.length, ' failed.');
+		if (jasmineStatus === 'done') {
+			var jasmineTotalSpecs = page.evaluate(function () { return jsApiReporter.specs(); });
+			var jasmineFailedSpecs = jasmineTotalSpecs.filter(function (spec) { return spec.status !== 'passed'; });
+			console.log(jasmineTotalSpecs.length, 'specs executed,', jasmineFailedSpecs.length, 'failed.');
 			phantom.exit(jasmineFailedSpecs.length);
-		} else {
-			setInterval(checkLoop, 100);
 		}
+		setInterval(checkLoop, 100);
 	};
 
 if (args.length < 2) {
@@ -18,8 +22,8 @@ if (args.length < 2) {
 }
 
 page.open(args[1], function (status) {
-	if (status !== "success") {
-		console.log("Unable to access network");
+	if (status !== 'success') {
+		console.log('Unable to access network');
 		phantom.exit(1);
 	}
 	checkLoop();
